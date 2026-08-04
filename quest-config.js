5f888fb03563387031092c8c3c3932ec38e357eb
engoB <71884956+engoB@users.noreply.github.com>
2026-08-04 12:59:45 +0200

    Update quest-config.js

--- quest-config.js
+++ quest-config.js
@@ -16,23 +16,23 @@ window.QUEST_CONFIG = {
 
     // Pistes du Carnet d'Enquête (Dévoilées UNIQUEMENT lorsque l'étape est active !)
     preRiddles: {
-        1: "📖 <strong>Piste Cuivre (Signal Hex) :</strong> Décodez la trame Hexadécimale : <code>46 69 67 68 74 20 43 6C 75 62</code>. Localisez ce film de 1999 (139 min, réalisé par David Fincher) tiré du roman de 1996 de Chuck Palahniuk.",
-        2: "📖 <strong>Piste Jade (Signal Morse) :</strong> Décodez le Morse : <code>-- .- - .-. .. -..-</code>. Localisez le 1er volet exact de la saga (1999, 136 min, réalisé par les Wachowski).",
-        3: "📖 <strong>Piste Cristal (OSINT / Coordonnées) :</strong> Coordonnées GPS du laboratoire de lancement : <code>33.9425° N, 118.4081° W</code>. Localisez l'épopée spatiale de Christopher Nolan (2014, 169 min).",
+        1: "📖 <strong>Piste Cuivre (OSINT) :</strong> Décodez la trame Hexadécimale : <code>46 69 67 68 74 20 43 6C 75 62</code>. Localisez ce film de 1999 (139 min, réalisé par David Fincher) tiré du roman de 1996 de Chuck Palahniuk.",
+        2: "📖 <strong>Piste Jade (Chronos) :</strong> Décodez le Morse : <code>-- .- - .-. .. -..-</code>. Localisez le 1er volet exact de la saga (1999, 136 min, réalisé par les Wachowski).",
+        3: "📖 <strong>Piste Cristal (Astrophysique) :</strong> Coordonnées GPS du laboratoire de lancement : <code>33.9425° N, 118.4081° W</code>. Localisez l'épopée spatiale de Christopher Nolan (2014, 169 min).",
         4: "🥚 <strong>Piste Ultime :</strong> Les 3 clés sont réunies ! L'Œuf d'Or s'est matérialisé dans le synopsis du film 'Inception' (2010)."
     },
 
     // Énigmes de Clé (Accessibles UNIQUEMENT sur la fiche du bon film cible)
     riddles: {
-        1: "🔑 <strong>Énigme de Cuivre :</strong> Combinez en MAJUSCULES : [PRENOM NOM DU PERSONNAGE DE BRAD PITT] + [NOM DE L'ENTREPRISE FICTIVE DU SAVON]. <br><em>Format requis : TYLER DURDEN PAPER STREET</em>",
-        2: "🗝️ <strong>Énigme de Jade :</strong> Combinez en MAJUSCULES : [PILULE CHOISIE PAR NEO EN 2 MOTS] + [NOM DE LA DERNIÈRE CITÉ HUMAINE]. <br><em>Format requis : PILULE ROUGE ZION</em>",
-        3: "💎 <strong>Énigme de Cristal :</strong> Combinez en MAJUSCULES : [NOM DU TROU NOIR GEANT] + [NOM DU DR EXPLORATEUR DE LA PLANÈTE DE GLACE]. <br><em>Format requis : GARGANTUA MANN</em>"
+        1: "🔑 <strong>Énigme de Cuivre :</strong> Combinez en MAJUSCULES : [PRENOM NOM DU PERSONNAGE DE BRAD PITT] + [ANNEE DU FILM]. <br><em>Format requis : TYLER DURDEN 1999</em>",
+        2: "🗝️ <strong>Énigme de Jade :</strong> Combinez en MAJUSCULES : [OBJET DE MORPHEUS EN 2 MOTS] + [HEXADÉCIMAL DU MOT NEO]. <br><em>Format requis : PILULE ROUGE 4E 45 4F</em>",
+        3: "💎 <strong>Énigme de Cristal :</strong> Combinez en MAJUSCULES : [NOM DU DR EXPLORATEUR DE LA PLANÈTE DE GLACE] + [DILATATION TEMPORELLE : 1 HEURE = X ANNEES]. <br><em>Format requis : MILLER 7 ANNEES</em>"
     },
 
     // Empreintes cryptographiques SHA-256 strictes des réponses
     hashes: {
-        1: "a4d701e88e31ff73a9e6d0a75f850e0503f191b988f615462dddf5e12f62776c", // TYLER DURDEN PAPER STREET
-        2: "85cbfbf5d70b3b9b4f63c89ffb53f6631eefc5e317c2f06bd0fbb10e3d0db99b", // PILULE ROUGE ZION
-        3: "e439211c1eeb6b3c2025ad87a87ea312014ea8ec76bb6cb1c49bcf20d41e737c"  // GARGANTUA MANN
+        1: "d5c9c3e60be1c8b3684a44f475510688dd61f0ef53cf68b1c4e7fb2fb1f10738", // TYLER DURDEN 1999
+        2: "03f905cbf5d487299a9a08e121e7d23d8c11e7e40854d19aa4e16d414e86a1cf", // PILULE ROUGE 4E 45 4F
+        3: "b845a72f2e5bb2720b0bd324d081b37b420ee9b19e918451838d7bb6d7a4cb40"  // MILLER 7 ANNEES
     }
 };
